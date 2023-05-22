fdb58a39a68154b94ce4925b05194d50b4bd5cda
Michal Zamboch <zamboch.michal@seznam.cz>
2023-05-22 17:03:27 +0200

    dualboot_grub_fix.sh

---
+++ fedora/grub_fix.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo dnf install grub-customizer
+
+grubConfig="/etc/default/grub"
+searchString="GRUB_ENABLE_BLSCFG=\"true\""
+replaceString="GRUB_ENABLE_BLSCFG=\"false\""
+
+finalExperssion=s/"$searchString"/"$replaceString"/
+echo $finalExperssion
+
+sudo sed -i '$finalExperssion' $grubConfig
+sudo grub2-mkconfig -o /boot/efi/EFI/fedora/grub.cfg
