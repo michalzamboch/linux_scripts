07d53364ff6cbdf763bf94ffa1ee18c58b92b426
Michal Žamboch <57073535+michalzamboch@users.noreply.github.com>
2023-05-22 17:19:05 +0200

    Update grub_fix.sh

--- fedora/grub_fix.sh
+++ fedora/grub_fix.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-sudo dnf install grub-customizer
+sudo dnf -y update
+sudo dnf -y install grub-customizer
 
 grubConfig="/etc/default/grub"
 searchString="GRUB_ENABLE_BLSCFG=true"
 replaceString="GRUB_ENABLE_BLSCFG=false"
 
 finalExperssion=s/"$searchString"/"$replaceString"/
-echo $finalExperssion
-
 sudo sed -i "$finalExperssion" $grubConfig
-sudo grub2-mkconfig -o /boot/efi/EFI/fedora/grub.cfg
+
+finalGrubConfig=/boot/efi/EFI/fedora/grub.cfg 
+sudo grub2-mkconfig -o $finalGrubConfig 
