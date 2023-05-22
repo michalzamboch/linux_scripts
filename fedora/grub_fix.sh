7c107e348986708651a9a20b41e0c6c618383719
Michal Žamboch <57073535+michalzamboch@users.noreply.github.com>
2023-05-22 17:09:15 +0200

    Update grub_fix.sh

--- fedora/grub_fix.sh
+++ fedora/grub_fix.sh
@@ -6,8 +6,8 @@ grubConfig="/etc/default/grub"
 searchString="GRUB_ENABLE_BLSCFG=true"
 replaceString="GRUB_ENABLE_BLSCFG=false"
 
-finalExperssion=s/"$searchString"/"$replaceString"/
+finalExperssion=s"$searchString""$replaceString"
 echo $finalExperssion
 
-sudo sed -i '$finalExperssion' $grubConfig
+sudo sed -i $finalExperssion $grubConfig
 sudo grub2-mkconfig -o /boot/efi/EFI/fedora/grub.cfg
