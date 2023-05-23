ef300fa556a5b6fef640e19a44eb6d72b637e367
Michal Žamboch <zamboch.michal@seznam.cz>
2023-05-23 08:55:10 +0200

    updated hibernation

--- fedora/enable_hibernation.sh
+++ fedora/enable_hibernation.sh
@@ -6,15 +6,16 @@ sudo swapon /dev/sda3
 partitionInfo=lsblk --noheadings -o UUID /dev/sda3
 uuid=grep -oP '(?<=<).*?(?=>)' <<< "$partitionInfo"
 
-resumeConfig="/etc/dracut.conf.d/"
+resumeConfig="/etc/dracut.conf.d/resume.conf"
 resumeInfo="add_dracutmodules+=\" resume \""
-echo $resumeInfo >> $resumeConfig
+sudo touch $resumeConfig
+sudo echo $resumeInfo >> $resumeConfig
 
 sudo dracut --regenerate-all --force
 sudo lsinitrd -m
 
 grubFile="/etc/default/grub"
 grubFileEntry="GRUB_CMDLINE_LINUX=\"[...] resume=UUID=<"$uuid">\""
-echo $grubFileEntry >> $grubFile
+sudo echo $grubFileEntry >> $grubFile
 
 sudo grub2-mkconfig -o /boot/grub2/grub.cfg
