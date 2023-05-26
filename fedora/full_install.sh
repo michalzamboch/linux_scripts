02879246b2262a8085fdabdd1da69f5f194f6f85
Michal Zamboch <zamboch.michal@seznam.cz>
2023-05-26 21:27:11 +0200

    Updated  full install script

--- fedora/full_install.sh
+++ fedora/full_install.sh
@@ -9,19 +9,13 @@ sudo dnf -y upgrade
 # RPM Fusion and Codecs
 sh ./rpm_fusion.sh
 sh ./install_codecs.sh
+sudo dnf -y update
 
 # DNF packages
 sh ./install_software.sh
 
 # Flatpaks
 sh ./activate_flatpaks.sh
-sh ../common/install_flatpaks.sh
-
-# Time system fix
-sh ../common/dualboot_time_fix.sh
-
-# Increase vm max map count
-sh ../common/increase_vm_max_map_count.sh
 
 # Grub fix
 sh ./grub_fix.sh
@@ -31,3 +25,23 @@ sh ./create_aliases.sh
 
 # Driver update
 sh ./driver_update.sh
+
+# Running scripts from common folder
+pushd .
+cd ../common/
+
+# Install flatpak packages
+sh ./install_flatpaks.sh
+
+# Time system fix
+sh ./dualboot_time_fix.sh
+
+# Increase vm max map count
+sh ./increase_vm_max_map_count.sh
+
+# Install Starship shell prompt
+sh ./font.sh
+sh ./install_starship.sh
+
+popd
+
