89fdd3d1bd6a08537c0fd82728034c5d111e98be
Michal Žamboch <zamboch.michal@seznam.cz>
2023-07-08 19:36:23 +0200

    Starship update

--- common/install_starship.sh
+++ common/install_starship.sh
@@ -2,6 +2,10 @@
 
 # https://starship.rs/
 
-curl -sS https://starship.rs/install.sh | sh
+if ! command -v starship &> /dev/null
+then
+  curl -sS https://starship.rs/install.sh | sh
+fi
+
 cat ./configs/starshipInit.txt >> $HOME/.bashrc
 source $HOME/.bashrc
