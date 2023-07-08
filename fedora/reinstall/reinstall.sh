49aaf35a140dc61e33cb59ff3d2c953f43294451
Michal Žamboch <zamboch.michal@seznam.cz>
2023-07-08 19:29:15 +0200

    Measuring installation time.

--- fedora/reinstall/reinstall.sh
+++ fedora/reinstall/reinstall.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-sudo sh ./su_install.sh
+start_time=$SECONDS
 
+sudo sh ./su_install.sh
 sh ./user_install.sh
 
-sudo reboot
+elapsed=$(( SECONDS - start_time ))
+
+eval "echo Installation time: $(date -ud "@$elapsed" +'$((%s/3600/24)) days %H hr %M min %S sec')"
+echo "Please, reboot the system now."
+
