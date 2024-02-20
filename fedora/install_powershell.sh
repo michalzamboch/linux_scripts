5109398074428b7d9a5f9b8b65b1fb170d1aa6c7
Michal Žamboch <zamboch.michal@seznam.cz>
2024-02-20 21:59:16 +0100

    Powershell. Clean comments.

---
+++ fedora/install_powershell.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# https://learn.microsoft.com/en-us/powershell/scripting/install/install-rhel?view=powershell-7.4
+
+sudo dnf install https://github.com/PowerShell/PowerShell/releases/download/v7.4.1/powershell-7.4.1-1.rh.x86_64.rpm
